bc4ccf95efb26f354dfea464eba0e247aef62127
Hope Bourne <habourne@hotmail.com>
2013-03-23 14:10:27 -0700

    Add User Model with Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Omrails::Application.routes.draw do
+  
+  devise_for :users
+
+  # devise_for :installs
+
  get 'about' => 'pages#about'
 
  root :to => 'pages#home'
@@ -59,4 +64,5 @@ Omrails::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
+  # devise_for :installs
 end
