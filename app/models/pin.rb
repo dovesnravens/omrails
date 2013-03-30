6bef7450119eaf52e95eb1240b68999204f263fb
Hope Bourne <habourne@hotmail.com>
2013-03-30 16:26:14 -0700

    Add Pins association with Users

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,6 +1,8 @@
 class Pin < ActiveRecord::Base
   attr_accessible :description
 
-  validates :description, presense: true
+  validates :description, presence: true
 
+  belongs_to :user
+  validates :user_id, presence: true
 end
